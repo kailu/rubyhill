3b55556817661e69c76a0313a928d094a2217d09
kailu <lukai1984@gmail.com>
2020-12-03 00:31:37 -0800

    Create check.js

---
+++ js/check.js
@@ -0,0 +1,39 @@
+/**
+     * Creates new cookie or removes cookie with negative expiration
+     * @param  key       The key or identifier for the store
+     * @param  value     Contents of the store
+     * @param  exp       Expiration - creation defaults to 30 days
+     */
+    function createCookie(key, value, exp) {
+        var date = new Date();
+        date.setTime(date.getTime() + (exp * 24 * 60 * 60 * 1000));
+        var expires = "; expires=" + date.toGMTString();
+        document.cookie = key + "=" + value + expires + "; path=/";
+    }
+    /**
+     * Returns contents of cookie
+     * @param  key       The key or identifier for the store
+     */
+    function readCookie(key) {
+        var nameEQ = key + "=";
+        var ca = document.cookie.split(';');
+        for (var i = 0, max = ca.length; i < max; i++) {
+            var c = ca[i];
+            while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
+    }
+    
+   function getParameterByName(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)'),
+    results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+   }
+   
+   var clickid = getParameterByName('clickid');
+   if(clickid){
+      createCookie('rubyhill_click', clickid, 30);
+   }
+   
